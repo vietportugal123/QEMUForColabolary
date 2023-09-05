16b2f535c79af152a42ff2cd56b16e8641b59364
vietportugal123 <141608478+vietportugal123@users.noreply.github.com>
2023-09-05 13:11:36 +0700

    Update Win7x64.sh

--- Win7x64.sh
+++ Win7x64.sh
@@ -1,7 +1,7 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 rm -rf ngrok-stable-linux-amd64.zip
-./ngrok authtoken 29Y7qXwzz01LI5RKuUvQgs7isKx_UWgYUeWwLZrLLZdYDTx1
+./ngrok authtoken 2AhzlBipiJUC2n9aAL92gzkOASi_Xw3H6GUx9qUKrro1STgF
 nohup ./ngrok tcp --region ap 5900 &>/dev/null &
 sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
@@ -9,7 +9,7 @@ sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
 qemu-img create -f raw windows764.img 32G
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
-wget -O windows764.iso 'https://dl.malwarewatch.org/windows/Windows%207.iso'
+wget -O windows764.iso 'https://go.microsoft.com/fwlink/p/?LinkID=2195443&clcid=0x409&culture=en-us&country=US'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
