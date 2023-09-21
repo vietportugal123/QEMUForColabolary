3de15403bdefdacb977aaabbe44e36ebc9ea1a21
vietportugal123 <141608478+vietportugal123@users.noreply.github.com>
2023-09-21 17:32:57 +0700

    Update Win7.sh

--- Win7.sh
+++ Win7.sh
@@ -1,7 +1,7 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-rm -rf ngrok-stable-linux-amd64.zip
-./ngrok authtoken 29Y7qXwzz01LI5RKuUvQgs7isKx_UWgYUeWwLZrLLZdYDTx1
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region ap 5900 &>/dev/null &
 sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
