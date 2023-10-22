3ae8506e5513510f2c648fda3604820683e39f9f
vietportugal123 <141608478+vietportugal123@users.noreply.github.com>
2023-10-22 13:15:12 +0700

    Create noVNC.sh

---
+++ noVNC.sh
@@ -0,0 +1,9 @@
+clear
+
+echo Cloning noVNC...
+git clone https://github.com/novnc/noVNC.git > /dev/null 2>&1
+wget https://raw.githubusercontent.com/vietportugal123/QEMUForColabolary/main/index.html > /dev/null 2>&1
+mv index.html noVNC/index.html
+echo Starting noVNC Server...
+nohup ./noVNC/utils/novnc_proxy --listen 80 --vnc localhost:5901 &>/dev/null &
+echo "All done! Please click + > View HTTP port 80 on Host 1 to access your VM!"
